8516e2e926c5650f00608d09dcd29f535055faf7
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-04 10:10:39 -0400

    change main banner

--- src/App.vue
+++ src/App.vue
@@ -41,10 +41,43 @@
       <!--  -->
     </v-navigation-drawer>
 
-    <v-app-bar app>
+   <!--  <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>MyBuji</v-toolbar-title>
+    </v-app-bar> -->
+    <!-- app bar changes-->
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="mbeach.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer =!drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>myBuji Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
